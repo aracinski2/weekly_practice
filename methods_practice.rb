4e2d6440555f3aaea9037b5948eb14162c9ef7e7
Alex Racinski <aracinski2@gmail.com>
2020-10-31 11:31:36 -0500

    problem 3

--- methods_practice.rb
+++ methods_practice.rb
@@ -1,11 +1,18 @@
-# 1
-puts "Please enter a word"
-answer = gets.chomp
-puts answer.upcase
+# # 1
+# puts "Please enter a word"
+# answer = gets.chomp
+# puts answer.upcase
 
-# 2
-puts "Please enter a number"
-answer = gets.chomp
-if answer.to_i > 100
-  puts "That's a big number"
-end
+# # 2
+# puts "Please enter a number"
+# answer = gets.chomp
+# if answer.to_i > 100
+#   puts "That's a big number"
+# end
+
+# 3
+puts "Please enter two numbers and I will add them."
+number_1 = gets.chomp
+number_2 = gets.chomp
+p number_1.to_i + number_2.to_i
+
