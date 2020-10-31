b3bd16a385d4fbfcd5c653d306178d265f9b421e
Alex Racinski <aracinski2@gmail.com>
2020-10-31 11:27:41 -0500

    problem 2

--- methods_practice.rb
+++ methods_practice.rb
@@ -2,3 +2,10 @@
 puts "Please enter a word"
 answer = gets.chomp
 puts answer.upcase
+
+# 2
+puts "Please enter a number"
+answer = gets.chomp
+if answer.to_i > 100
+  puts "That's a big number"
+end
