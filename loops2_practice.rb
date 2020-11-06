59cc2f999a770dd1b4ca26e4f54947e65627a04a
Alex Racinski <aracinski2@gmail.com>
2020-11-06 15:20:20 -0600

    loops practice complete

---
+++ loops2_practice.rb
@@ -0,0 +1,75 @@
+# #1
+# index = 1
+# while index <= 10
+# p index
+# index += 1
+# end
+
+# #2
+# index = 0
+# while index <= 4
+#   p "hello"
+#   index += 1
+# end
+
+# #3
+# while true
+#   puts "Please enter a word"
+#   word = gets.chomp
+#   if word == "stop"
+#     break
+#   end
+# end
+
+# #4
+# number = 0
+# while number <= 100
+#   p number
+#   number += 5
+# end
+
+# #5
+# index = 0
+# while index < 10
+#   p 9000
+#   index += 1
+# end
+
+# #6
+# while true
+#   puts "please enter a number"
+#   number = gets.chomp.to_i
+#   if number > 10
+#     break
+#   end
+# end
+
+# #7
+# number = 50
+# while number <= 70
+#   p number
+#   number += 1
+# end
+
+# #8
+# index = 0
+# while index < 144
+#   p "Around the world"
+#   index += 1
+# end
+
+# #9
+# while true
+#   p "please enter a word"
+#   word = gets.chomp
+#   if word.length > 5
+#     break
+#   end
+# end
+
+# #10
+# number = 2
+# while number <= 40
+#   p number
+#   number += 2
+# end
