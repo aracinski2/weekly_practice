e1ccf39ca5aa7e615f97f16516997bea234d4a4b
Alex Racinski <aracinski2@gmail.com>
2020-11-07 13:55:45 -0600

    string weekly practice done

---
+++ strings_practice.rb
@@ -0,0 +1,60 @@
+# # 1
+# x = "alex"
+# y = "r"
+# p x + " " + y
+
+# # 2
+# x = "alex"
+# y = "r"
+# p "#{x} #{y}"
+
+# # 3
+# p "word please"
+# input = gets.chomp
+# if input == "marco"
+#   p "polo"
+# end
+
+# # 4
+# a = "blue"
+# b = "red"
+# c = "green"
+# p a + " " + b + " " + c
+
+# # 5
+# a = "blue"
+# b = "red"
+# c = "green"
+# p "#{a} #{b} #{c}"
+
+# # 6
+# p "what is your name"
+# name = gets.chomp
+# if name != "santa"
+#   p "you are not santa"
+# end
+
+# # 7
+# title = "amazing"
+# author = "alex r"
+# p "my fav book is " + title + " by " + author + "!"
+
+# # 8
+# title = "amazing"
+# author = "alex r"
+# p "my fav book is #{title} by #{author}!"
+
+# # 9
+# p "what is your password"
+# pass = gets.chomp
+# if pass == "Joshua"
+#   p "Shall we play a game?"
+# else
+#   p "Access Denied"
+# end
+
+# 10
+a = "chicago"
+b = "new york"
+c = "LA"
+p a + " is where I live, but " + b + " is where i wanna visit." + c + " is where ive been."
