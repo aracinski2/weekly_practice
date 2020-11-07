2085e21a39e0db720036b1106ccb2f80a336248f
Alex Racinski <aracinski2@gmail.com>
2020-11-07 13:09:34 -0600

    second conditional problem

--- conditionals2_practice.rb
+++ conditionals2_practice.rb
@@ -1,10 +1,17 @@
-# 1
-number = 9
-if number == 10
-  p 0
-else 
+# # 1
+# number = 9
+# if number == 10
+#   p 0
+# else 
+#   p -1
+# end
+
+# 2
+number = 10
+if number < 10
   p -1
+elsif number > 10
+  p 1
+else
+  p 0
 end
-
-
-
