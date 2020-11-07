4b043a034c41ad09bc9de8038642f7aaa8ab2ba6
Alex Racinski <aracinski2@gmail.com>
2020-11-07 13:12:16 -0600

    conditional problem 3

--- conditionals2_practice.rb
+++ conditionals2_practice.rb
@@ -6,11 +6,20 @@
 #   p -1
 # end
 
-# 2
-number = 10
-if number < 10
-  p -1
-elsif number > 10
+# # 2
+# number = 10
+# if number < 10
+#   p -1
+# elsif number > 10
+#   p 1
+# else
+#   p 0
+# end
+
+# 3
+number_1 = 5
+number_2 = 11
+if number_1 < 10 && number_2 < 10
   p 1
 else
   p 0
