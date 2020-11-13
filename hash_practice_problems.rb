47db07285d1054bab9def1cfb85a3cf88c3797b2
Alex Racinski <aracinski2@gmail.com>
2020-11-13 14:48:32 -0600

    hashes problem 5

--- hash_practice_problems.rb
+++ hash_practice_problems.rb
@@ -20,9 +20,17 @@
 # hash[:beef] = 12
 # p hash
 
-# 4
-book = {title: "hello there", author: "Obi Wan", pages: 203, language: "english"}
-p book[:title]
-p book[:author]
-p book[:pages]
-p book[:language]
+# # 4
+# book = {title: "hello there", author: "Obi Wan", pages: 203, language: "english"}
+# p book[:title]
+# p book[:author]
+# p book[:pages]
+# p book[:language]
+
+# 5
+books = [
+  {title: "hello there", author: "Obi Wan", pages: 203, language: "english"},
+  {title: "goodbye", author: "darth", pages: 189, language: "englishhhh"},
+  {title: "wassup", author: "qui gon", pages: 120, language: "english maybe"}
+]
+p books[2][:author]
