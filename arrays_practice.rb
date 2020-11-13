844e4300430e906b38239882c74e6c09c14ecaf0
Alex Racinski <aracinski2@gmail.com>
2020-11-13 15:09:43 -0600

    arrays problem 4

--- arrays_practice.rb
+++ arrays_practice.rb
@@ -9,10 +9,17 @@
 # array[1] = 12
 # p array
 
-# 3
-array = [1, 2, 3, 4, 5]
-index = 0
-while index < array.length
-  p array[index]
-  index += 1
-end
+# # 3
+# array = [1, 2, 3, 4, 5]
+# index = 0
+# while index < array.length
+#   p array[index]
+#   index += 1
+# end
+
+# 4
+array = [1]
+array << 2
+array << 3
+array << 4
+p array
