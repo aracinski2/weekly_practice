1d2e685b609c6d12c8e28d4aa14c0dfb1244241a
Alex Racinski <aracinski2@gmail.com>
2020-11-13 14:38:58 -0600

    hashes second problem

--- hash_practice_problems.rb
+++ hash_practice_problems.rb
@@ -1,5 +1,12 @@
-# 1
-hash = {first: "jeff", last: "bloom", email: "jeffbloom@gmail.com"}
-p hash[:first]
-p hash[:last]
-p hash[:email]
+# # 1
+# hash = {first: "jeff", last: "bloom", email: "jeffbloom@gmail.com"}
+# p hash[:first]
+# p hash[:last]
+# p hash[:email]
+
+# 2
+array = [
+  {first: "jeff", last: "bloom", email: "jeffbloom@gmail.com"},{first: "jason", last: "meyers", email: "jjason@gmail.com"},{first: "john", last: "jude", email: "jude@gmail.com"}
+]
+p array[0][:first]
+p array[0][:last]
