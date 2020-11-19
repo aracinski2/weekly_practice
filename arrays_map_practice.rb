267d84e290e83d78fd4f0894f6d999a3e568bd01
Alex Racinski <aracinski2@gmail.com>
2020-11-19 13:40:58 -0600

    add while, .each, .map solutions to arrays map practice problem 1

---
+++ arrays_map_practice.rb
@@ -0,0 +1,19 @@
+# # 1
+array = [1, 2, 3]
+new_array = []
+# index = 0
+# while index < array.length
+#   new_array << array[index] * 3
+#   index +=1
+# end
+# p new_array
+
+# array.each do |number|
+#   new_array << number * 3
+# end
+# p new_array
+
+# new_array = array.map do |number|
+#   number * 3
+# end
+# p new_array
