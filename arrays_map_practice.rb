ab874094e87085cc0307c0e29789cf7823f8ef99
Alex Racinski <aracinski2@gmail.com>
2020-11-20 16:39:47 -0600

    arrays map practice while, each, and map solution problem 2

--- arrays_map_practice.rb
+++ arrays_map_practice.rb
@@ -1,6 +1,6 @@
 # # 1
-array = [1, 2, 3]
-new_array = []
+# array = [1, 2, 3]
+# new_array = []
 # index = 0
 # while index < array.length
 #   new_array << array[index] * 3
@@ -17,3 +17,23 @@ new_array = []
 #   number * 3
 # end
 # p new_array
+
+# 2
+array = ["hello", "goodbye"]
+# new_array = []
+# index = 0
+# while index < array.length
+#   new_array << array[index].upcase
+#   index += 1
+# end
+# p new_array
+
+# array.each do |string|
+#   new_array << string.upcase
+# end
+# p new_array
+
+new_array = array.map do |string|
+  string.upcase
+end
+p new_array
