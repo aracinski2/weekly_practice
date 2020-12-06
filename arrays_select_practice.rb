35f9e70b9b6ad245be782c87cb8f48deab8efc12
Alex Racinski <aracinski2@gmail.com>
2020-12-05 18:51:49 -0600

    solution with while, .each, .select to array select practice problem 2

--- arrays_select_practice.rb
+++ arrays_select_practice.rb
@@ -1,6 +1,6 @@
 # 1
-numbers = [2, 32, 80, 18, 12, 3]
-numbers_less = []
+# numbers = [2, 32, 80, 18, 12, 3]
+# numbers_less = []
 
 # index = 0
 # while index < numbers.length
@@ -18,5 +18,30 @@ numbers_less = []
 # end
 # p numbers_less
 
-numbers_less = numbers.select {|number| number < 20}
-p numbers_less
+# numbers_less = numbers.select {|number| number < 20}
+# p numbers_less
+
+# 2
+strings = ["winner", "winner", "chicken", "dinner"]
+w_strings = []
+# index = 0
+# while index < strings.length
+#   string = strings[index]
+#   if string[0].downcase == "w"
+#     w_strings << string
+#   end
+#   index += 1
+# end
+# p w_strings
+
+# strings.each do |string|
+#   if string[0] == "w"
+#     w_strings << string
+#   end
+# end
+# p w_strings
+
+w_strings = strings.select do |string|
+  string[0] == "w"
+end
+p w_strings
