117654ddf4610b8449dbd66b8c3bccb81152bb61
Alex Racinski <aracinski2@gmail.com>
2020-12-05 19:10:55 -0600

    solution with while, .each, .select to array select practice problem 5

--- arrays_select_practice.rb
+++ arrays_select_practice.rb
@@ -72,8 +72,8 @@
 # p pricey_items
 
 # 4
-numbers = [2, 4, 5, 1, 8, 9, 7]
-even_numbers = []
+# numbers = [2, 4, 5, 1, 8, 9, 7]
+# even_numbers = []
 # index = 0
 # while index < numbers.length
 #   number = numbers[index]
@@ -91,7 +91,32 @@ even_numbers = []
 # end
 # p even_numbers
 
-even_numbers = numbers.select do |number|
-  number.even?
+# even_numbers = numbers.select do |number|
+#   number.even?
+# end
+# p even_numbers
+
+# 5
+strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+short_strings = []
+# index = 0
+# while index < strings.length
+#   string = strings[index]
+#   if string.length < 4
+#     short_strings << string
+#   end
+#   index += 1
+# end
+# p short_strings
+
+# strings.each do |string|
+#   if string.length < 4
+#     short_strings << string
+#   end
+# end
+# p short_strings
+
+short_strings = strings.select do |string|
+  string.length < 4
 end
-p even_numbers
+p short_strings
