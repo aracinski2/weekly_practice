36311020d4b5ed0fd9bd27d07a7d14b6a5d295f6
Alex Racinski <aracinski2@gmail.com>
2020-12-12 21:17:18 -0600

    converting data practice problem 3

--- converting_data_practice.rb
+++ converting_data_practice.rb
@@ -7,9 +7,23 @@
 # p hash
 
 # 2
-array = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+# array = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+# hash = Hash.new
+# array.each do |index|
+#   hash[index[:id]] = index
+# end
+# p hash
+
+# 3
+string = "bookkeeper"
 hash = Hash.new
-array.each do |index|
-  hash[index[:id]] = index
+index = 0
+while index < string.length
+  letter = string[index]
+  if hash[letter] == nil
+    hash[letter] = 0
+  end
+  hash[letter] += 1
+  index += 1
 end
 p hash
