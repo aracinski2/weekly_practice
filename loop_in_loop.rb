7c5a273e99a7820e768ec660172790940126afcc
Alex Racinski <aracinski2@gmail.com>
2021-02-05 18:52:55 -0600

    loop with a loop problem 4

--- loop_in_loop.rb
+++ loop_in_loop.rb
@@ -28,19 +28,38 @@
 # p combined_strings
 
 # 3
-array = ["a", "b", "c", "d"]
-combined_strings = []
+# array = ["a", "b", "c", "d"]
+# combined_strings = []
+# index = 0
+# while index < array.length
+#   index2 = 0
+#   while index2 < array.length
+#     if array[index] == array[index2]
+      
+#     else
+#       combined_strings << array[index] + array[index2]
+#     end
+#     index2 += 1
+#   end
+#   index += 1
+# end
+# p combined_strings
+
+# 4
+array = [5, -2, 1, -9, -7, 2, 6]
+largest = 0
 index = 0
 while index < array.length
   index2 = 0
   while index2 < array.length
-    if array[index] == array[index2]
-      
-    else
-      combined_strings << array[index] + array[index2]
+    if array[index] != array[index2]
+      product = array[index] * array[index2]
+      if product > largest
+        largest = product
+      end
     end
     index2 += 1
   end
   index += 1
 end
-p combined_strings
+p largest
