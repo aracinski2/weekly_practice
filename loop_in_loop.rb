9dea82ee934eaa6b4119f896ebeea86edc6569a3
Alex Racinski <aracinski2@gmail.com>
2021-02-05 18:36:12 -0600

    loop with a loop problem 2

--- loop_in_loop.rb
+++ loop_in_loop.rb
@@ -1,13 +1,28 @@
 # 1 
-array = [[1, 3], [8, 9], [2, 16]]
-flat_array = []
+# array = [[1, 3], [8, 9], [2, 16]]
+# flat_array = []
+# index = 0
+# while index < array.length
+#   index2 = 0
+#   while index2 < array[index].length
+#     flat_array << array[index][index2]
+#     index2 +=1
+#   end
+#   index +=1
+# end
+# p flat_array
+
+# 2
+array1 = ["a", "b", "c"]
+array2 = ["d", "e", "f", "g"]
+combined_strings = []
 index = 0
-while index < array.length
+while index < array1.length
   index2 = 0
-  while index2 < array[index].length
-    flat_array << array[index][index2]
-    index2 +=1
+  while index2 < array2.length
+    combined_strings << array1[index] + array2[index2]
+    index2 += 1
   end
-  index +=1
+  index += 1
 end
-p flat_array
+p combined_strings
