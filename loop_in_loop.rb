31f9640c9194dd981bd500b6bf154735e8ed0735
Alex Racinski <aracinski2@gmail.com>
2021-02-05 18:47:27 -0600

    loop with a loop problem 3

--- loop_in_loop.rb
+++ loop_in_loop.rb
@@ -13,14 +13,32 @@
 # p flat_array
 
 # 2
-array1 = ["a", "b", "c"]
-array2 = ["d", "e", "f", "g"]
+# array1 = ["a", "b", "c"]
+# array2 = ["d", "e", "f", "g"]
+# combined_strings = []
+# index = 0
+# while index < array1.length
+#   index2 = 0
+#   while index2 < array2.length
+#     combined_strings << array1[index] + array2[index2]
+#     index2 += 1
+#   end
+#   index += 1
+# end
+# p combined_strings
+
+# 3
+array = ["a", "b", "c", "d"]
 combined_strings = []
 index = 0
-while index < array1.length
+while index < array.length
   index2 = 0
-  while index2 < array2.length
-    combined_strings << array1[index] + array2[index2]
+  while index2 < array.length
+    if array[index] == array[index2]
+      
+    else
+      combined_strings << array[index] + array[index2]
+    end
     index2 += 1
   end
   index += 1
