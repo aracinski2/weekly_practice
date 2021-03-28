d3ed8805437aab9a93a6e6738f2c21f5d3c37005
Alex Racinski <aracinski2@gmail.com>
2021-03-28 15:24:09 -0500

    nested arrays practice problem 2

--- nested_arrays_practice.rb
+++ nested_arrays_practice.rb
@@ -1,13 +1,28 @@
 # 1
-array = [[1, 3], [8, 9], [2, 16]]
+# array = [[1, 3], [8, 9], [2, 16]]
+# index = 0
+# flat_array = []
+# while index < array.length
+#   index2 = 0
+#   while index2 < array[index].length
+#     flat_array << array[index][index2]
+#     index2 += 1
+#   end
+#   index += 1
+# end
+# p flat_array
+
+# 2
+array = ["a", "b", "c"]
+array_2 = ["d", "e", "f", "g"]
 index = 0
-flat_array = []
+combined_strings = []
 while index < array.length
   index2 = 0
-  while index2 < array[index].length
-    flat_array << array[index][index2]
+  while index2 < array_2.length
+    combined_strings << array[index] + array_2[index2]
     index2 += 1
   end
   index += 1
 end
-p flat_array
+p combined_strings
